94be8c6bfeb749a0d9e66f1bb1d873a9825167ab
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-23 10:49:19 +0430

    init

--- src/Photo.js
+++ src/Photo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Photo = ({urls:{regular}, alt_description, likes, user:{name, portfolio_url, profile_url:{medium}}}) => {
+const Photo = ({urls:{regular}, alt_description, likes, user:{name, portfolio_url, profile_image:{medium}}}) => {
   return <article className='photo'>
     <img src={regular} alt={alt_description} />
     <div className="photo-info">
