51afe3941645b7a3463dce8df1c5b3d93eee47a8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-23 18:37:35 +0430

    init

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ function App() {
     <div className="search">
       <form className="search-form">
         <input type="text" placeholder='search' className='form-input'/>
+        <button type='submit'></button>
       </form>
     </div>
   </main>
