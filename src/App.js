3702a9533d258df957542b83dc58d9fbf8135072
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-23 14:16:21 +0430

    init

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const fetchImages = async () => {
     let url;
-    url = `${mainUrl}?clientID=46CvFkwGy7FL3zJh4ua9XROfFxdCGATsGUcrXtg8YJw`
+    url = `${mainUrl}?
+    clientID=46CvFkwGy7FL3zJh4ua9XROfFxdCGATsGUcrXtg8YJw`
     try {
       const response = await fetch(url)
+      const data = await response.json()
     } catch (error) {
-      
+      console.log(error);
     }
   }
   
