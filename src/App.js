f4aeac23ad7f2d8feeb3658900f601120fcea4bc
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-23 14:30:55 +0430

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
