9f283ba5b09c443ab8416348b7e0a629a4148cf0
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-23 10:50:15 +0430

    init

--- src/Photo.js
+++ src/Photo.js
@@ -5,7 +5,7 @@ const Photo = ({urls:{regular}, alt_description, likes, user:{name, portfolio_ur
     <img src={regular} alt={alt_description} />
     <div className="photo-info">
       <h4>{name}</h4>
-      <p>{likes}</p>
+      <p>{likes} likes</p>
     </div>
   </article>
 }
