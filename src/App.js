cd87f8971f1762b35ab0519d0419603be902a711
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-23 14:48:37 +0430

    init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const fetchImages = async () => {
     setLoading(true)
     let url;
-    url = `${mainUrl}?client_id=46CvFkwGy7FL3zJh4ua9XROfFxdCGATsGUcrXtg8YJw`
+    url = `${mainUrl}${clientID}`
     try {
       const response = await fetch(url)
       const data = await response.json()
