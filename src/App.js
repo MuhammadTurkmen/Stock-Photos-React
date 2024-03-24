c9e965a8fd2de52a2ef00fcfc49c978b3c58f905
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-24 06:10:03 +0430

    init

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ function App() {
 
   useEffect(() => { 
     const event = window.addEventListener('scroll', () => { 
-      if((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
-
+      if((window.innerHeight + window.scrollY) >= document.body.scrollHeight - 2) {
+        console.log('it work');
       } 
     }) 
   
