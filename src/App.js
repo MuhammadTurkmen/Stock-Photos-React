151c8f0a23056ecb6ccf72c4eacb256b70df2e3a
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-24 07:51:40 +0430

    init

--- src/App.js
+++ src/App.js
@@ -58,7 +58,11 @@ function App() {
   return <main>
     <section className="search">
       <form className="search-form">
-        <input type="text" placeholder='search' className='form-input' value={query} onChange={(e) => setQuery(e.target.value)}/>
+        <input 
+          type="text" 
+          placeholder='search' 
+          className='form-input' 
+          value={query} onChange={(e) => setQuery(e.target.value)}/>
         <button type='submit' className='submit-btn' onClick={handleSubmit}>
           <FaSearch />
         </button>
